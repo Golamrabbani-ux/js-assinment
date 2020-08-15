cfd0e7897ac6a6bb9587733d19005b3fe6788e4c
Your Name <61206200+Golamrabbani-ux@users.noreply.github.com>
2020-08-15 23:46:22 +0600

    Finished program solving assinment

--- assinment.js
+++ assinment.js
@@ -5,10 +5,10 @@ function feetToMile(feet){
     }
     else{
         var mile = (feet / 5280).toFixed(5);
-        // console.log(mile);
+        // console.log("Mile: ", mile);
     }
 }
-miles = feetToMile(-1);
+miles = feetToMile(1);
 
 
 // Wood Calculator
@@ -24,8 +24,41 @@ function woodCalculator(chair, table, bed){
         // console.log("Please Enter Positive Number.....");
     }
 }
-woodCalculator(1, 1, 1);
+woodCalculator(1, 5, 10);
 
 
+//Brick Calculator
 
+function brickCalculator( floor){
+    if(floor <= 10){
+        brick = (floor * 15)*1000
+    }
+    else if(floor <= 15 ||10 >floor){
+         brick = (floor * 12)*1000
+    }
+    else{
+         brick = (floor * 10)*1000
+    }
+    return brick
+   } 
+
+var totalBrick = brickCalculator(16)
+// console.log(totalBrick)
+
+//Tiny Friends
+function tinyFriend(friends){
+    var shortName = friends[0];
+    for(var i=0; i<friends.length; i++){
+        var friendName = friends[i];
+        if(shortName.length > friendName.length){
+            shortName = friendName
+        }
+    }
+    return shortName;
+}
+
+
+var allFriends = ["Abir", "Kobir", "G.Rabbani", "Raihan", "Ali", "Nabila"];
+var result = tinyFriend(allFriends);
+console.log(result);
 
